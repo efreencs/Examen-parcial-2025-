dde99bda6e7bde42ee9594c48b97b8733cd2c64d
efreencs <efrencostas2005@gmail.com>
2025-10-31 09:26:59 +0100

    commit

--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,9 @@
 console.log('tot ok');
 
-const apiUrl = 'https://pokeapi.co/api/docs/v2';
+const apiUrl = 'https://pokeapi.co/api/v2';
 let equip = [];
 
-window.onload = () => {
+window.onload = async() => {
 
     const botoBuscar = document.getElementById('botobuscar');
     botoBuscar.addEventListener('click', () => buscarPokemon());
@@ -14,22 +14,34 @@ window.onload = () => {
     for (const actual of pokemons) {
         const pokemondiv = document.createElement('div');
         pokemondiv.classList.add('pokemondiv');
+
+        const name = document.createElement('p');
+        name.innerText = actual.name;
+        pokemondiv.appendChild(name);
+
+
+        const id = document.createElement('p');
+        id.innerText = `ID: ${actual.id}`;
+        pokemondiv.appendChild(id);
+
     }
+
 }
 
 
 async function fetchpokemons() {
-    const response = await fetch(apiUrl);
+    const response = await fetch(`${apiUrl}/pokemon?limit=151`);
     const data = await response.json();
-
     const pokemondetails = [];
+
     for (const pokemon of data.results) {
-        const details = await fetchpokemon(pokemon.url);
+        const details = await fetch(pokemon.url);
         const pokemondata = await details.json();
         pokemondetails.push(pokemondata);
     }
     return pokemondetails;
 }
+
 
 async function buscarPokemon() {
     const pokemonbuscar = document.getElementById('buscarpokemon').value;
@@ -37,5 +49,24 @@ async function buscarPokemon() {
     const resposta = document.getElementById('resultats');
 
     const httppokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${nompokemon}`);
+
+    if (!httppokemon.ok) {
+        resposta.innerHTML = 'Pokemon no trobat. Si us plau, intenta-ho de nou.';
+        return;
+    }
+
+    const dadespokemon = await httppokemon.json();
+
+    const aliaspokemon = document.createElement('h3');
+    aliaspokemon.innertext = dadespokemon.name;
+    resposta.appendChild(aliaspokemon);
+
+    const imatgepokemon = document.createElement('img');
+    imatgepokemon.src = dadespokemon.sprites.front_default;
+    resposta.appendChild(imatgepokemon);
+
+
+
+
     
 }
